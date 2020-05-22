57c1a462e132243f3c34803bff721ffd0252726e
Elvis Ng <eng18@ucsc.edu>
2020-05-21 20:26:33 -0700

    asset update

--- assets/tree.tsx
+++ assets/tree.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="New Piskel (2)" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../Downloads/New Piskel (2).png" width="96" height="96"/>
+<tileset version="1.2" tiledversion="1.3.4" name="Tree" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="Tree.png" width="64" height="96"/>
 </tileset>
