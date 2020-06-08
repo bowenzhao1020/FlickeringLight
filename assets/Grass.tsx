3c63309f1f7a90b78ef6596532db355cc8bf7fb1
Elvis Ng <eng18@ucsc.edu>
2020-06-07 22:34:41 -0700

    map/other update

--- assets/Grass.tsx
+++ assets/Grass.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="Grass" tilewidth="32" tileheight="32" tilecount="72" columns="6">
+ <editorsettings>
+  <export target="Grass.json" format="json"/>
+ </editorsettings>
  <image source="Grass.png" width="192" height="384"/>
 </tileset>
