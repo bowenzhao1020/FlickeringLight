e1afca277b28608054bd5c060c166678e37e2744
Elvis Ng <eng18@ucsc.edu>
2020-06-07 19:47:26 -0700

    change

--- assets/Grass.tsx
+++ assets/Grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="Grass" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="Grass.png" width="96" height="96"/>
+<tileset version="1.2" tiledversion="1.3.4" name="Grass" tilewidth="32" tileheight="32" tilecount="72" columns="6">
+ <image source="Grass.png" width="192" height="384"/>
 </tileset>
